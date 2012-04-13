e0fdeae78cdb7b17b76f6b7c8f449c3a548ff899
Andres MRM <andresmrm@gmail.com>
2012-04-13 17:33:48 -0300

    aprimorada interface

--- jogocartas/views.py
+++ jogocartas/views.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from jinja2 import FileSystemLoader, Environment
 from slimish_jinja import SlimishExtension
 
+from jogo import *
+
 template_dirs = getattr(settings,'TEMPLATE_DIRS')
 default_mimetype = getattr(settings, 'DEFAULT_CONTENT_TYPE')
 env = Environment(loader=FileSystemLoader(template_dirs), extensions=[SlimishExtension])
@@ -16,28 +18,27 @@ def render_to_response(filename, context={},mimetype=default_mimetype):
     return HttpResponse(rendered,mimetype=mimetype)
 
 
-
-class Jogador():
-
-    def __init__(self):
-        self.nome = "Tolo"
-        self.dinheiro = 5
-        self.mao = []
-        self.mesa = {}
-
-
-
 def index(request):
     users = [{'name': 'foo', 'last_name': 'bar'},
              {'name': 'bar', 'middle_name': 'baz'},
              {'name': 'baz'}]
 
     j1 = Jogador()
+    j1.nome = "Tolo1"
     j1.mao = [1,2,3,4]
+    j1.mesa = { 'vermelho':[1,2,3,4,5,6,7,8,9],
+                'azul':[1,2],
+                'amarelo': [1],
+                'verde': [1],
+                'roxo':[1]} 
     j2 = Jogador()
+    j2.nome = "Tolo2"
     j3 = Jogador()
+    j3.nome = "Tolo3"
     j4 = Jogador()
+    j4.nome = "Tolo4"
     j5 = Jogador()
-    jogadores = [ j.__dict__ for j in [j2, j2, j3, j4, j5]]
+    j5.nome = "Tolo5"
+    jogadores = [ j.__dict__ for j in [j1, j2, j3, j4, j5]]
 
     return render_to_response('jogo.slim',{'jogadores':jogadores})
